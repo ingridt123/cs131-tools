c35f4d0f6860723572cd72e201be7fec7b0d3d49
ingridt123 <ingridszenga@gmail.com>
2021-02-25 18:02:47 +0800

    Add convert dot to png

--- tree-visualization/AST.hs
+++ tree-visualization/AST.hs
@@ -2,31 +2,37 @@ module AST where
 
 import Dot
 import Data.Text as T
+import System.Process
 
 -- TODO: look at encodeCompass / CardinalDirection in dot hackage
--- TODO: convert to pdf (Terminal: dot files.dot -Tpng > files.png)
+-- TODO: fix (" " ++ label ++ " ") otherwise Node not "Node" in dot file
 
-createDotFile :: [Char] -> DotGraph -> IO ()
-createDotFile fileName dotGraph = do
-  putStrLn $ "dumping example dotgraph to " ++ target fileName
-  encodeToFile (target fileName) dotGraph
+createASTFiles :: [Char] -> DotGraph -> IO ProcessHandle
+createASTFiles fileName dotGraph = do
+  putStrLn $ "Dumping dotgraph to " ++ targetDot fileName
+  putStrLn $ "Dumping png to " ++ targetPng fileName
+  encodeToFile (targetDot fileName) dotGraph
+  runCommand ("dot " ++ targetDot fileName ++ " -Tpng > " ++ targetPng fileName)
 
 
-target :: [Char] -> FilePath
-target fileName = fileName ++ ".dot"
+targetDot :: String -> FilePath
+targetDot fileName = fileName ++ ".dot"
 
-convertToId :: [Char] -> Id
+targetPng :: String -> FilePath
+targetPng fileName = fileName ++ ".png"
+
+convertToId :: String -> Id
 convertToId id = Id (T.pack id)
 
-convertToNodeId :: [Char] -> NodeId 
+convertToNodeId :: String -> NodeId 
 convertToNodeId id = NodeId (convertToId id) Nothing
 
-createNode :: [Char] -> [Char] -> Statement
+createNode :: String -> String -> Statement
 createNode id label = 
     StatementNode $ NodeStatement (convertToNodeId id) 
-                                  [Attribute (convertToId "label") (convertToId label)]
+                                  [Attribute (convertToId "label") (convertToId (" " ++ label ++ " "))]
 
-createEdge :: [Char] -> [Char] -> Statement 
+createEdge :: String -> String -> Statement 
 createEdge id1 id2 =
     StatementEdge $ EdgeStatement (ListTwo (EdgeNode (convertToNodeId id1)) 
                                            (EdgeNode (convertToNodeId id2)) 
